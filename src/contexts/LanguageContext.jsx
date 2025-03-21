f74379c353db3d6a2dd6f9967a26f0ba317d179a
Söùrãv <souravpramanick1997@gmail.com>
2025-03-21 21:00:42 +0530

    Language context debug

--- src/contexts/LanguageContext.jsx
+++ src/contexts/LanguageContext.jsx
@@ -1,10 +1,15 @@
 import { createContext, useEffect, useState } from "react";
 
 const LanguageContext = createContext();
-
+/*eslint-disable-next-line react/prop-types*/
 function LanguageProvider({ children }) {
-  const [language, setLanguage] = useState(localStorage.getItem("language") || "hindi");
+  const [language, setLanguage] = useState(localStorage.getItem("language") || "english");
   useEffect(()=>{
+    if(language !=='english' && language!=='hindi'){
+      setLanguage("english")
+      localStorage.setItem('language',"english")
+      return 
+    }
     localStorage.setItem('language',language)
   },[language])
   return (
