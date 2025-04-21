56ce013705044f4bb9a9e44ea7d44694a34e020a
Söùrãv <souravpramanick1997@gmail.com>
2025-04-21 19:52:51 +0530

    postgres config updated

--- config/postgres.js
+++ config/postgres.js
@@ -2,15 +2,15 @@ const { Pool } = require("pg");
 require("dotenv").config()
 
 const pool = new Pool({
-    // user:process.env.DB_USER,
-    // host:process.env.DB_HOST,
-    // database:process.env.DB_NAME,
-    // password:process.env.DB_PASSWORD,
-    // port:process.env.DB_PORT
-    connectionString : process.env.POSTGRES_URL,
-    ssl: {
-        rejectUnauthorized: false, 
-      },
+    user:process.env.DB_USER,
+    host:process.env.DB_HOST,
+    database:process.env.DB_NAME,
+    password:process.env.DB_PASSWORD,
+    port:process.env.DB_PORT
+    // connectionString : process.env.POSTGRES_URL,
+    // ssl: {
+    //     rejectUnauthorized: false, 
+    //   }
 })
 
 module.exports = pool
